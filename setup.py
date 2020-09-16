ef561f7eb32783710124d0ab4a789d46292a9a35
Johannes <2505558w@student.gla.ac.uk>
2020-09-16 14:57:38 +0200

    changed setup requirements

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     ],
     cmdclass     = cmdclass,
     ext_modules  = ext_modules,
-    install_requires = ['numpy', 'Cython' 'numpydoc', 'sphinx', 'math']
+    install_requires = ['numpy', 'Cython', 'sphinx', 'math']
 )
 
